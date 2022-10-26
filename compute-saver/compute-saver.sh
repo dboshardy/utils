92295444d00ff372d55d8d2b91d062cfb2a83ec4
Drew Boshardy <dboshardy@gmail.com>
2022-10-26 08:55:53 -0500

    change script to check for user logged in

--- compute-saver/compute-saver.sh
+++ compute-saver/compute-saver.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 FILE=/var/DO_NOT_STOP
-do_not_stop=1
+do_not_stop=0
+user_logged_in=0
 if [ -f "$FILE" ]; then
-  do_not_stop=0
+  do_not_stop=1
 fi
-if [ $do_not_stop -eq 0 ]; then
+user=$(who | grep dboshardy)
+if [ -z "$user" ]; then
+  user_logged_in=1
+fi
+if [ $do_not_stop -eq 1 ]; then
   echo "Training is running, do not stop the instance."
   exit 0
-elif [ $do_not_stop -eq 1 ]; then
+elif [ $do_not_stop -eq 0 ]; then
+  if [ $user -eq 1 ]; then
+    echo "User is logged in, do not stop the instance." # short circuit
+    exit 0
+  fi
   echo "Training is not running, stop the instance."
   gcloud compute instances stop "$instance" --project "$project" --zone "$zone"
   exit 0
